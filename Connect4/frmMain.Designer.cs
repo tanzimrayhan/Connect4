5361f0766e16133a71073f2ac8b775997453e145
Tanzim Ali <tanzim_rayhan@yahoo.com>
2018-05-10 08:43:00 -0400

    Works but still bugs left

---
+++ Connect4/frmMain.Designer.cs
@@ -0,0 +1,93 @@
+﻿namespace Connect4
+{
+    partial class frmMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnExit = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnRestart = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnExit
+            // 
+            this.btnExit.Location = new System.Drawing.Point(1696, 901);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(100, 65);
+            this.btnExit.TabIndex = 0;
+            this.btnExit.Text = "Exit";
+            this.btnExit.UseVisualStyleBackColor = true;
+            this.btnExit.Click += new System.EventHandler(this.btnExit_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Rockwell", 9F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(448, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(92, 17);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "CONNECT 4";
+            // 
+            // btnRestart
+            // 
+            this.btnRestart.Location = new System.Drawing.Point(1540, 899);
+            this.btnRestart.Name = "btnRestart";
+            this.btnRestart.Size = new System.Drawing.Size(125, 68);
+            this.btnRestart.TabIndex = 2;
+            this.btnRestart.Text = "New Game";
+            this.btnRestart.UseVisualStyleBackColor = true;
+            this.btnRestart.Click += new System.EventHandler(this.btnRestart_Click);
+            // 
+            // frmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.PaleTurquoise;
+            this.ClientSize = new System.Drawing.Size(1832, 1037);
+            this.Controls.Add(this.btnRestart);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnExit);
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Name = "frmMain";
+            this.Text = "Connect Four";
+            this.Load += new System.EventHandler(this.frmMain_Load);
+            this.Paint += new System.Windows.Forms.PaintEventHandler(this.frmMain_Paint);
+            this.MouseClick += new System.Windows.Forms.MouseEventHandler(this.frmMain_MouseClick);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnExit;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnRestart;
+    }
+}
+
---
+++ Connect4/frmMain.cs
@@ -0,0 +1,189 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Connect4
+{
+    public partial class frmMain : Form
+    {
+        private Rectangle[] boardColumns;
+        private int[,] board;
+        private int turn;
+
+        public frmMain()
+        {
+            InitializeComponent();
+            //FormBorderStyle = FormBorderStyle.None;
+            WindowState = FormWindowState.Maximized;
+
+
+            boardColumns = new Rectangle[7];
+            board = new int[6, 7];
+            turn = 1;
+            DoubleBuffered = true;
+        }
+
+        private void frmMain_Paint(object sender, PaintEventArgs e)
+        {
+
+            e.Graphics.FillRectangle(Brushes.Black, 104, 104, 1200, 680);
+            for (int i = 0; i < 6; i++)
+            {
+                for (int j = 0; j < 7; j++)
+                {
+                    if (i == 0)
+                    {
+                        boardColumns[j] = new Rectangle(150 + 170 * j, 48, 100, 680);
+                    }
+                    e.Graphics.FillEllipse(Brushes.White, 150 + 170 * j, 120 + 100* i, 75, 75);
+                }
+            }
+        }
+
+        private void frmMain_MouseClick(object sender, MouseEventArgs e)
+        {
+            int columIndex = ColumnNumber(e.Location);
+            if (columIndex != -1)
+            {
+                int rowIndex = EmptyRow(columIndex);
+                if (rowIndex != -1)
+                {
+                    board[rowIndex, columIndex] = turn;
+                    if (turn == 1)
+                    {
+                        Graphics g = CreateGraphics();
+                        g.FillEllipse(Brushes.Red, 150 + 170 * columIndex, 120 + 100 * rowIndex, 75, 75);
+                    }
+                    else if (turn == 2)
+                    {
+                        Graphics g = CreateGraphics();
+                        g.FillEllipse(Brushes.Yellow, 150 + 170 * columIndex, 120 + 100 * rowIndex, 75, 75);
+
+                    }
+                    int winner = WinningPlayer(turn);
+                    if (winner != -1)
+                    {
+                        string player = (winner == 1) ? "Red" : "Blue";
+                        MessageBox.Show("Congratulations! " + player + " Player has won!");
+                        Application.Restart();
+                    }
+
+                    if (turn == 1)
+                    {
+                        turn = 2;
+                    }
+                    else turn = 1;
+                }
+            }
+        }
+
+        private int WinningPlayer(int playerToCheck)
+        {
+            // Vertical check (|)
+            for (int row = 0; row < board.GetLength(0)-3; row++)
+            {
+                for (int col = 0; col < board.GetLength(1); col++)
+                {
+                    if (NumbersEqual(playerToCheck, board[row, col], board[row+1, col], board[row+2, col], board[row+3, col]))
+                    {
+                        return playerToCheck;
+                    }
+                }
+            }
+
+            // Horizontal check (-)
+            for (int row = 0; row < board.GetLength(0); row++)
+            {
+                for (int col = 0; col < board.GetLength(1)-3; col++)
+                {
+                    if (NumbersEqual(playerToCheck, board[row, col], board[row, col+1], board[row, col+2], board[row, col+3]))
+                    {
+                        return playerToCheck;
+                    }
+                }
+            }
+
+            // top-left diagonal win check (\)
+            for (int row = 0; row < board.GetLength(0) -3; row++)
+            {
+                for (int col = 0; col < board.GetLength(1) - 3; col++)
+                {
+                    if (NumbersEqual(playerToCheck, board[row, col], board[row + 1, col + 1], board[row + 2, col + 2], board[row + 3, col + 3]))
+                    {
+                        return playerToCheck;
+                    }
+                }
+            }
+
+            // top-right diagonal win check (\)
+            for (int row = 0; row < board.GetLength(0) - 3; row++)
+            {
+                for (int col = 3; col < board.GetLength(1) - 3; col++)
+                {
+                    if (NumbersEqual(playerToCheck, board[row, col], board[row + 1, col - 1], board[row + 2, col - 2], board[row + 3, col -3]))
+                    {
+                        return playerToCheck;
+                    }
+                }
+            }
+
+            return -1;
+        }
+
+        private bool NumbersEqual(int toCheck, params int[] numbers)
+        {
+            foreach (int num in numbers)
+            {
+                if (num != toCheck)
+                {
+                    return false;
+                }
+            }return true;
+        }
+
+        private int ColumnNumber(Point mouse)
+        {
+            for (int i = 0; i < boardColumns.Length; i++)
+            {
+                if ((mouse.X >= boardColumns[i].X) && (mouse.Y >= boardColumns[i].Y))
+                {
+                    if((mouse.X <= boardColumns[i].X + boardColumns[i].Width) && (mouse.Y <= boardColumns[i].Y + boardColumns[i].Height))
+                    {
+                        return i;
+                    }
+                }
+            }return -1;
+        }
+
+        private int EmptyRow(int col)
+        {
+            for (int i = 5; i >= 0 ; i--)
+            {
+                if (board[i, col] == 00)
+                    return i;
+            }
+            return -1;
+        }
+
+        private void frmMain_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnExit_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void btnRestart_Click(object sender, EventArgs e)
+        {
+            Application.Restart();
+        }
+    }
+}
